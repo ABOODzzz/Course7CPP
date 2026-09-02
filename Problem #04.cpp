f93321b1d95ee3525966ca59457de1b1eb52a1c1
ABOOD <A.Aljamal2@gju.edu.jo>
2026-09-02 22:52:29 +0300

    Create Problem #04.cpp

---
+++ Problem #04.cpp
@@ -0,0 +1,96 @@
+//code 1 to adding the colums elements 
+#include<iostream>
+#include<cstdlib>
+#include<iomanip>
+const int row = 3,colm=3;
+using namespace std;
+int RandomNum(int from, int to) {
+	return rand() % (to - from + 1) + from;
+	
+}
+
+int ReadPosNum(string message) {
+	int num = 1;
+	do {
+		cout << message; cin >> num; cout << endl;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			num = -1;
+			cout << "\n\nre_entere pos num : "; cin >> num;
+		}
+		 if (num < 0) {
+			cout << "\nNumber must be non-negative.\n";
+		}
+	} while (num < 0);
+	
+	return num;
+}
+void FillArrayWithRandomNums(int array[3][3]) {
+	int from = ReadPosNum("enter from : ");
+	int to = ReadPosNum("enter to : ");
+	if (from > to)swap(from, to);
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < colm; j++) {
+			array[i][j] = RandomNum(from, to);
+		}
+	}
+}
+void PrintArrayRandomNums(int array[3][3]) {
+	cout << "\n\nthw following is a 3*3 random matrix:" << endl;
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < colm; j++) {
+			cout << setw(4) << array[i][j] << "     ";
+		}cout << endl;
+	}
+}
+
+int SumOfArray(int array[3][3],int row) {
+	int sum = 0;
+	
+		for (int j = 0;j<colm;j++) {
+			sum += array[row][j];
+		}
+	
+	return sum;
+}
+void SwapArray(int array[row][colm],int array2[row][colm]) {
+	for (int i = 0;i<row;i++) {
+		for (int j = 0;j<colm;j++) {
+			array2[j][i] = array[i][j];
+		}
+	}
+}
+
+void ArraySumArray(int array[3][3],int array1[row]) {
+	
+	
+	int localRow = 0;
+	for (int i = 0;i<row;i++) {
+		array1[i] = SumOfArray(array, localRow);
+		localRow++;
+	}
+}
+
+void PrintArray(int array1[row]) {
+	for (int i = 0;i<row;i++) {
+		cout << "\nthe sum of row " << i + 1 << " is :" << array1[i] << endl;
+	}
+}
+
+int main() {
+	srand((unsigned)time(NULL));
+	
+	int array[3][3];
+	FillArrayWithRandomNums(array);
+	PrintArrayRandomNums(array);
+	int array1[row];
+	ArraySumArray(array,array1);
+	PrintArray(array1);
+	cout << "\n\nswaped array is : " << endl;
+	int arraySwaped[row][colm] = {0};
+	SwapArray(array,arraySwaped);
+	PrintArrayRandomNums(arraySwaped); cout << endl;
+	ArraySumArray(arraySwaped,array1);
+	PrintArray(array1);
+}
